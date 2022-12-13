243ad46898cc6d7961137d4f1dd253601ee399a7
Paul-Wonder <onyejelapaula@gmail.com>
2022-12-12 21:51:57 -0800

    check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#inclu#include <stdio.h>
 /**
-* main - Entry
-* Return: Always 0
+* main -Print a line of code using puts
+*
+* Return: 0
 */
-int main(void)
 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#inc#include <stdio.h>
 /**
-* main - Entry
-* Return: Always 0
+* main - print a c program using printf
+*
+* Return: 0
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
