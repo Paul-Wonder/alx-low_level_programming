f7870daeb32d11af08b8ab075ef7cf25beccb237
Paul-Wonder <onyejelapaula@gmail.com>
2022-12-14 21:08:44 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value
+ * @n: argument
+ * Return: returns abs value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+}
